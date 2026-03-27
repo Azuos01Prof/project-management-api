4f0e85d1e0d97ea8dde1fa78059c56b569c952a5
ProfMarcosSouza <marcossouza01@prof.educacao.sp.gov.br>
2026-03-27 00:32:21 -0300

    Create jest.config.js

---
+++ .github/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'server.js',
+    '!**/node_modules/**',
+    '!**/coverage/**'
+  ],
+  testMatch: ['**/tests/**/*.test.js'],
+  verbose: true,
+  testTimeout: 10000
+};
